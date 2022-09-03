0770145f71967d9d7a0e377f9dece92209753c3b
Mlth <94927866+Mlth@users.noreply.github.com>
2022-09-03 12:33:54 +0200

    New branch (#3)
    Created user interface for IsLeapYear

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -1,2 +1,12 @@
-// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+namespace MyApp;
+
+public class Program{
+    public static void Main(String[] args){
+        var calculator = new LeapYearCalculator();
+        
+        Console.Write("Write a year: ");
+        int userYear = Convert.ToInt32(Console.ReadLine());
+
+        calculator.IsLeapYear(userYear);
+    }
+}
