4b073d40eac519c49a9176811518d0e69f631a4e
Mads Lücking Thingvad <mlth@itu.dk>
2022-09-03 12:29:14 +0200

    Created user interface for IsLeapYear

--- MyApp/LeapYearCalculator.cs
+++ MyApp/LeapYearCalculator.cs
@@ -3,8 +3,10 @@ namespace MyApp;
 public class LeapYearCalculator {
     public bool IsLeapYear(int year){
         if(year%4 == 0 && (year%100 != 0 || year%400 == 0)){
+            Console.WriteLine("yay");
             return true;
         }
+        Console.WriteLine("nay");
         return false;
     }
 }
--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -1,1 +1,12 @@
-﻿Console.WriteLine("Hello, World!");
+﻿namespace MyApp;
+
+public class Program{
+    public static void Main(String[] args){
+        var calculator = new LeapYearCalculator();
+        
+        Console.Write("Write a year: ");
+        int userYear = Convert.ToInt32(Console.ReadLine());
+
+        calculator.IsLeapYear(userYear);
+    }
+}
